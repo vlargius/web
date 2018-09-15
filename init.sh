353b447478cc1472307a4e7e6a2f93db6e878c1d
selfdev <vlargius@gmail.com>
2018-09-15 23:34:57 +0300

    move to box

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 new_conf_path=/etc/nginx/sites-enabled/test.conf
 
 sudo rm -rf $new_conf_path
-sudo ln -s ~/projects/web/etc/nginx.conf $new_conf_path
+sudo ln -s  /home/box/web/etc/nginx.conf $new_conf_path
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo ln -s ./etc/gunicorn.conf /etc/gunicorn.d/test
