79c0d8bbcbb69650ccbe685f57cedaa785279e74
selfdev <vlargius@gmail.com>
2018-09-15 23:04:28 +0300

    fix

---
+++ README.md
@@ -0,0 +1,2 @@
+simple deploy configuration for nginx server
+
--- init.sh
+++ init.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+new_conf_path=/etc/nginx/sites-enabled/test.conf
+
+sudo rm -rf $new_conf_path
+sudo ln -s ~/projects/web/etc/nginx.conf $new_conf_path
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start
+#sudo ln -s ./etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/mysql start
 
 
